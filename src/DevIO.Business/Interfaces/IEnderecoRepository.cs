e077a901776058e7c5171305462e55654b11ca0a
Willian Gonçalves de Jesus <will_kaiker@hotmail.com>
2024-07-12 13:29:42 -0300

    Acessando o banco via repositórios

---
+++ src/DevIO.Business/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using DevIO.Business.Models;
+using System.Threading.Tasks;
+using System;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
